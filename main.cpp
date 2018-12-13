6e85f50e5f6781abbf784b77b223150dad2d143b
SadAnthony <sedletskiy.anton@gmail.com>
2018-12-13 07:44:46 +0300

    dvdvds

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
-int main () {
+double funcVelocity(double r, double e, double m);
+double funcRange(double v);
 
-    return 0;
+int main() {
+    double dt = 0.01;
+    double v[1001];
+    v[0] = 0.5;
+    double x[1001];
+    x[0] = 1.05;
+
+    for (int i = 0; i < 1000; i++) {
+        v[i+1] = v[i] + dt*funcVelocity(x[i], 1/4, 1);
+        x[i+1] = x[i] + dt*funcRange(v[i]);
+        cout << "v = " << v[i] << ", x = " << x[i] << endl;
+    }
+}
+
+double funcVelocity(double r, double e, double m) {
+    return (-4*e/m * (-12/pow(r, 13) - (-6/pow(r, 7))));
+}
+
+double funcRange(double v) {
+    return v;
 }
