16e6ef83738d38b4b803abca3e210a0d6c843f43
SadAnthony <sedletskiy.anton@gmail.com>
2018-12-13 08:17:05 +0300

    Реализовал одномерный случай

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
+#include <iomanip>
 
 using namespace std;
 
@@ -8,21 +9,22 @@ double funcVelocity(double r, double e, double m);
 double funcRange(double v);
 
 int main() {
-    double dt = 0.01;
+    double h = 0.01;
     double v[1001];
-    v[0] = 0.5;
+    v[0] = 0.2;
     double x[1001];
-    x[0] = 1.05;
+    x[0] = 1.34;
 
     for (int i = 0; i < 1000; i++) {
-        v[i+1] = v[i] + dt*funcVelocity(x[i], 1/4, 1);
-        x[i+1] = x[i] + dt*funcRange(v[i]);
-        cout << "v = " << v[i] << ", x = " << x[i] << endl;
+        v[i+1] = v[i] + h*funcVelocity(x[i], 0.25, 1.0);
+        x[i+1] = x[i] + h*funcRange(v[i]);
+        cout << "v = " << fixed << setprecision(3)<< v[i]
+        << "\t x = " << x[i] << endl;
     }
 }
 
 double funcVelocity(double r, double e, double m) {
-    return (-4*e/m * (-12/pow(r, 13) - (-6/pow(r, 7))));
+    return (-4.0*e/m * (-12.0/pow(r, 13.0) - (-6.0/pow(r, 7.0))));
 }
 
 double funcRange(double v) {
