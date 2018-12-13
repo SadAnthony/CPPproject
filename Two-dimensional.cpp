220918cc3521ed20d70b5bb18a9910a1aaaa746a
SadAnthony <sedletskiy.anton@gmail.com>
2018-12-13 12:12:29 +0300

    dvdvds

--- Two-dimensional.cpp
+++ Two-dimensional.cpp
@@ -1,35 +1,77 @@
 #include <iostream>
-#include <cstdlib>
 #include <cmath>
 #include <iomanip>
 
 using namespace std;
 
+void potential(double h);
 double funcVelocity(double r, double e, double m);
 double funcRange(double v);
 
 int main() {
-    double h = 0.01, r;
-    double vx[1001], x[1001], vy[1001], y[1001];
-    vx[0] = 0.2;
-    x[0] = 1.34;
-    vy[0] = -0.2;
-    y[0] = 1.34;
+    double h = 0.01;
+    cout << "\tFor step = " << h << endl;
+    potential(h);
+    h = 0.02;
+    cout << "\tFor step = " << h << endl;
+    potential(h);
+    h = 0.05;
+    cout << "\tFor step = " << h << endl;
+    potential(h);
+    h = 0.1;
+    cout << "\tFor step = " << h << endl;
+    potential(h);
+    h = 0.2;
+    cout << "\tFor step = " << h << endl;
+    potential(h);
+    return 0;
+}
 
-    for (int i = 0; i < 1000; i++) {
-        r = sqrt(x[i] * x[i] + y[i] * y[i]);
+void potential(double h) {
+    const double m = 1.0;
+    const double e = 0.25;
 
-        vx[i+1] = vx[i] + h*funcVelocity(r, 0.25, 1.0);
-        x[i+1] = x[i] + h*funcRange(vx[i]);
+    double yExperiment = 0, xExperiment = 0;
+    double xPrev, x, y, yPrev, vxPrev, vyPrev, vx, vy;
+    vx = 0.0;
+    x = 3.0;
+    vy = sqrt(-1/m*x*funcVelocity(x, e, m));
+    y = 0.0;
 
-        vy[i+1] = vy[i] + h*funcVelocity(r, 0.25, 1.0);
-        y[i+1] = y[i] + h*funcRange(vy[i]);
+    const double t1 = 3.14/2*x/vy;
+    const double xTheory = 0.0;
+    const double yTheory = x;
 
-        cout << fixed << setprecision(3)
+
+    double r;
+    for (int i = 0; h * i < t1; i++) {
+        xPrev = x;
+        yPrev = y;
+        vxPrev = vx;
+        vyPrev = vy;
+        r = sqrt(x * x + y * y);
+
+        vx = vxPrev + h*funcVelocity(r, e, m) * (xPrev / r);
+        x = xPrev + h*funcRange(vxPrev);
+
+        vy = vyPrev + h*funcVelocity(r, e, m) * (yPrev / r);
+        y = yPrev + h*funcRange(vyPrev);
+
+        /*cout << fixed << setprecision(3)
             << "t = " << i*h
             << "\tvx = " << vx[i] << "\tx = " << x[i]
             << "\tvy = " << vy[i] << "\ty = " << y[i] << endl;
+        */
     }
+
+    xExperiment = x;
+    yExperiment = y;
+
+    //cout << "x experimental = " << xExperiment << "\ty experimental = " << yExperiment << endl;
+    cout << fixed << setprecision(4) << "x error = " << abs(xExperiment - xTheory)
+    << "\ty error = " << abs(yExperiment - yTheory) << endl;
+
+    //return 0;
 }
 
 double funcVelocity(double r, double e, double m) {
