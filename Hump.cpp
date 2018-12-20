d384fea02fadb08f2a066bb2bca0a9c4389ac872
SadAnthony <sedletskiy.anton@gmail.com>
2018-12-20 12:06:06 +0300

    Дописал уравнение переноса

--- Hump.cpp
+++ Hump.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
+#include <vector>
 
 using namespace std;
 
 double function(double x);
-void leftCorner();
+void leftCorner(double dx);
 
 int main() {
-    double h = 0.01;
-    cout << "\tFor step = " << h << endl;
-    leftCorner();
+    double h = 0.8;
+    cout << fixed << setprecision(3);
+    cout << "For step = " << h;
+    leftCorner(h);
+    h = 0.5;
+    cout << "For step = " << h;
+    leftCorner(h);
+    h = 0.3;
+    cout << "For step = " << h;
+    leftCorner(h);
+    h = 0.1;
+    cout << "For step = " << h;
+    leftCorner(h);
+    h = 0.05;
+    cout << "For step = " << h;
+    leftCorner(h);
+    h = 0.01;
+    cout << "For step = " << h;
+    leftCorner(h);
+    h = 0.005;
+    cout << "For step = " << h;
+    leftCorner(h);
+    h = 0.003;
+    cout << "For step = " << h;
+    leftCorner(h);
     return 0;
 }
 
@@ -18,21 +41,38 @@ double function(double x) {
     return ((x >= -3 && x <= 3) ? 1 : 0);
 }
 
-void leftCorner() {
+void leftCorner(double dx) {
     const double e = 0.5;
-    const double dx = 0.5;
-    const double dt = 0.5;
+    const double dt = 0.5*dx/e;
 
-    double xCurr = -20, xPrev;
-    double yCurr[1000], yPrev[1000];
-    yCurr[0] = function(xCurr);
+    vector<double> funcCurr;
+    vector<double> funcPrev;
 
-    for (int i = 0; i*dt < 5; i++) {
-            xPrev = xCurr;
-            yPrev[i] = yCurr[i];
-            yCurr[i+1] = (1 - e*dt/dx) * function(xCurr-i*dt) +
-                    (e*dt/dx) * function(xCurr-(i-1) * dt);
+    double x0 = -20;
+    int i, j;
 
+    for (i = 0; x0 + i*dx < 20; i++) {     //function for t = 0 and x = -20...+20
+        funcCurr.push_back(function(x0 + i*dx));
     }
 
+    funcPrev = funcCurr;
+
+    for (j = 0; j*dt < 10; j++) {
+        for (i = 0; x0 + i*dx < 20; i++) {
+            funcCurr[i] = (1 - e*dt/dx) * funcPrev[i] + e*dt/dx * funcPrev[i-1];
+        }
+        funcPrev = funcCurr;
+    }
+
+    /*for (i = 0; x0 + i*dx < 20; i++) {
+        cout << fixed << setprecision(3)
+        << "at x = " << x0+i*dx << "\t f = " << funcCurr[i]
+        << "\t f theor at t final = " << function(x0 + i*dx - e*j*dt) << endl;
+    }*/
+
+    double sum = 0;
+    for (i = 0; x0 + i*dx < 20; i++) {
+        sum += dx*abs(funcCurr[i] - function(x0 + i*dx - e*j*dt));
+    }
+    cout << "\tError = " << sum << endl;
 }
--- Two-dimensional.cpp
+++ Two-dimensional.cpp
@@ -81,3 +81,84 @@ double funcVelocity(double r, double e, double m) {
 double funcRange(double v) {
     return v;
 }
+
+/*
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+double function(double x);
+void leftCorner(double dx);
+
+int main() {
+    double h = 0.8;
+    cout << "\tFor step = " << h << endl;
+    leftCorner(h);
+    h = 0.5;
+    cout << "\tFor step = " << h << endl;
+    leftCorner(h);
+    h = 0.3;
+    cout << "\tFor step = " << h << endl;
+    leftCorner(h);
+    h = 0.1;
+    cout << "\tFor step = " << h << endl;
+    leftCorner(h);
+    h = 0.05;
+    cout << "\tFor step = " << h << endl;
+    leftCorner(h);
+    h = 0.01;
+    cout << "\tFor step = " << h << endl;
+    leftCorner(h);
+    h = 0.005;
+    cout << "\tFor step = " << h << endl;
+    leftCorner(h);
+    h = 0.0001;
+    cout << "\tFor step = " << h << endl;
+    leftCorner(h);
+    return 0;
+}
+
+double function(double x) {
+    return ((x >= -3 && x <= 3) ? 1 : 0);
+}
+
+void leftCorner(double dx) {
+    const double e = 0.5;
+    const double dt = 0.5*dx/e;
+
+    vector<double, double> func; //Prev value of f and Curr value
+    double fCurr[1000], fPrev[1000];
+
+
+    double x0 = -20;
+    int i, j;
+
+    for (i = 0; x0 + i*dx < 20; i++) {     //function for t = 0 and x = -20...+20
+        fCurr[i] = fPrev[i] = function(x0 + i*dx);
+    }
+
+    for (j = 0; j*dt < 10; j++) {
+        for (i = 0; x0 + i*dx < 20; i++) {
+            fCurr[i] = (1 - e*dt/dx) * fPrev[i] + e*dt/dx * fPrev[i-1];
+        }
+        for (i = 0; x0 + i*dx < 20; i++) {
+            fPrev[i] = fCurr[i];
+        }
+    }
+
+    for (i = 0; x0 + i*dx < 20; i++) {
+        /*cout << fixed << setprecision(3)
+        << "at x = " << x0+i*dx << "\t f = " << fCurr[i]
+        << "\t f theor at t final = " << function(x0 + i*dx - e*j*dt) << endl;
+}
+
+double sum = 0;
+for (i = 0; x0 + i*dx < 20; i++) {
+sum += dx*abs(fCurr[i] - function(x0 + i*dx - e*j*dt));
+}
+cout << "Error = " << sum << endl;
+}
+ */
